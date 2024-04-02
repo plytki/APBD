a59f47ddb73fa71cecd252c3480867bd81d86374
Paweł Cichocki <53957038+plytki@users.noreply.github.com>
2024-04-02 21:41:11 +0200

    cw 4

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        private readonly ClientRepository _clientRepository;
+        private readonly UserCreditService _userCreditService;
+
+        public UserService()
+        {
+            _clientRepository = new ClientRepository();
+            _userCreditService = new UserCreditService();
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!IsValidName(firstName, lastName) || !IsValidEmail(email) || !IsAdult(dateOfBirth))
+            {
+                return false;
+            }
+
+            var client = _clientRepository.GetById(clientId);
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            if (!IsValidUser(user))
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private bool IsValidName(string firstName, string lastName)
+        {
+            return !string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName);
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains('@') && email.Contains('.');
+        }
+
+        private bool IsAdult(DateTime dateOfBirth)
+        {
+            var age = CalculateAge(dateOfBirth);
+            return age >= 21;
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            return age;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName,
+                HasCreditLimit = true
+            };
+
+            SetCreditLimit(user, client.Type);
+
+            return user;
+        }
+
+        private void SetCreditLimit(User user, string clientType)
+        {
+            switch (clientType)
+            {
+                case "VeryImportantClient":
+                    user.HasCreditLimit = false;
+                    break;
+                case "ImportantClient":
+                    user.CreditLimit = CalculateCreditLimit(user) * 2;
+                    break;
+                default:
+                    user.CreditLimit = CalculateCreditLimit(user);
+                    break;
+            }
+        }
+
+        private int CalculateCreditLimit(User user)
+        {
+            return _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+        }
+
+        private bool IsValidUser(User user)
+        {
+            return !user.HasCreditLimit || user.CreditLimit >= 500;
+        }
+    }
+
+}
